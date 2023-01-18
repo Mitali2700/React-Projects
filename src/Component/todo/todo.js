4315fafe61af6cece844e9aefdfe92dce6ea2f6f
Mitali2700 <mitaligamer2002@gmail.com>
2023-01-18 21:51:44 +0530

    todo-list project started

---
+++ src/Component/todo/todo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./style.css";
+
+const todo = () => {
+  return (
+    <>
+        <div className='main_div'>
+            <div className='child-div'>
+            <figure>
+                <img src="" alt="" />
+            </figure>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default todo
